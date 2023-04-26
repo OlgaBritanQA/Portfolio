75bdf116a173ba748e3717b28de1f919ba136a3e
Olga Britan <britan.helga@yandex.ru>
2023-04-26 11:45:15 +0300

    файлы распределены по папкам

--- SQL/task_sql_JOIN.sql
+++ SQL/task_sql_JOIN.sql
@@ -4,12 +4,12 @@ id	first_name	last_name		id	adress	rooms
 		Таблица - view				
 		iapartment_id	client_id	date		
 
-Составить запрос, который выдаст список фамилий клиентов, записанных на просмотр 2х и более 3х комнатных квартир 
+Составить запрос, который выдаст список фамилий клиентов, записанных на просмотр 2х и более 3-х комнатных квартир 
 
-SELECT last_name
+SELECT client.last_name
 FROM client
 INNER JOIN view ON client.id=view.client_id
 INNER JOIN appartment ON view.appartment_id=appartment.id
-WHERE appartment.rooms>3
- GROUP BY last_name
+WHERE appartment.rooms=3
+GROUP BY last_name
 HAVING COUNT(view.client_id)>=2;					
